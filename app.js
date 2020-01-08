fd16ee72954fbea5067e0be0a1c44ada1b2d3e7e
stacy1930 <35913788+stacy1930@users.noreply.github.com>
2020-01-08 19:40:26 +0100

    connexion

--- app.js
+++ app.js
@@ -5,3 +5,25 @@ const app = express();
 
 const http = require("http").createServer(app);
 const io = require("socker.io")(http);
+
+// const Gant = require(path.join(_dirname, "Gant", "gantBdd.js"));
+
+// Connexion a la bdd
+const mongo = require("mongodb");
+const mongoClient = mongo.MongoClient;
+const url = "mongodb://localhost:27017/bddGantt";
+
+//Connexion a la bdd bddGant et creation de la collection
+mongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  let dbo = db.db("bddGantt");
+  dbo.createCollection("gantt", function(err, res) {
+    if (err) throw err;
+    console.log("Collection Created!");
+  });
+
+  dbo.collection("gantt").find({}, function(err, result) {
+    if (err) throw err;
+    // console.log(result.pseudo + " " + result.message);
+  });
+});
