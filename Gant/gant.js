31929c0b0cc9637fe3baa6ea2cd26ccfa7f9e0b2
stacy1930 <35913788+stacy1930@users.noreply.github.com>
2020-01-09 20:42:39 +0100

    gant

--- Gant/gant.js
+++ Gant/gant.js
@@ -1,4 +1,38 @@
 // On récupère ce que le client "dit"
-// const socket = io();
-// let titre = "Aucun titre";
-// let description = "Aucune description";
+const socket = io();
+let title = "Aucun titre";
+let description = "Aucune description";
+
+$("#addTitle").submit(function(e) {
+  e.preventDefault();
+  if ($("#title").val() == "") {
+    title = "Aucun titre";
+  } else {
+    title = $("#title").val();
+  }
+
+  $("#title").val("");
+  return false;
+});
+
+$("#addDesc").submit(function(e) {
+  e.preventDefault();
+  if ($("#description").val() == "") {
+    description = "Aucune description";
+  } else {
+    description = $("#description").val();
+  }
+
+  $("#description").val("");
+  return false;
+});
+
+//A FINIR STACY
+$("#addGroupTask").submit(function(e) {
+  e.preventDefault();
+  if ($("#gtN").val() == "") {
+    groupTaskName = "Aucun nom";
+  } else {
+    groupTaskName = $("#gtN");
+  }
+});
