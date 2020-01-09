c261fcf3a2136d0b51b026cff364d47047832c80
stacy1930 <35913788+stacy1930@users.noreply.github.com>
2020-01-09 22:44:45 +0100

    gant resource et grouptask

--- Gant/gant.js
+++ Gant/gant.js
@@ -4,6 +4,7 @@ let title = "Aucun titre";
 let description = "Aucune description";
 socket.emit("connection", "a user connected");
 
+// *******************TITRE******************
 $("#addTitle").submit(function(e) {
   e.preventDefault();
   if ($("#title").val() == "") {
@@ -15,6 +16,8 @@ $("#addTitle").submit(function(e) {
   $("#title").val("");
   return false;
 });
+
+// *******************DESCRIPTION******************
 
 $("#addDesc").submit(function(e) {
   e.preventDefault();
@@ -28,12 +31,46 @@ $("#addDesc").submit(function(e) {
   return false;
 });
 
-//A FINIR STACY
+// *******************GROUPTASK******************
 $("#addGroupTask").submit(function(e) {
   e.preventDefault();
   if ($("#gtN").val() == "") {
     groupTaskName = "Aucun nom";
   } else {
     groupTaskName = $("#gtN");
+  }
+
+  if ($("#gtS").val() == "") {
+    groupTaskStart = "";
+  } else {
+    groupTaskStart = $("gtS");
+  }
+
+  if ($("#gtE").val() == "") {
+    groupTaskEnd = "";
+  } else {
+    groupTaskEnd = $("gtE");
+  }
+});
+
+// *******************RESSOURCES******************
+$("#addResource").submit(function(e) {
+  e.preventDefault();
+  if ($("#rN").val() == "") {
+    resourceName = "Aucun nom";
+  } else {
+    groupTaskName = $("#rN");
+  }
+
+  if ($("#rC").val() == "") {
+    resourceCost = "";
+  } else {
+    resourceCost = $("rC");
+  }
+
+  if ($("#rT").val() == "") {
+    resourceType = "";
+  } else {
+    resourceType = $("rT");
   }
 });
