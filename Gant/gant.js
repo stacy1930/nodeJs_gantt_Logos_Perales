18f0d6bcebb989743a88c3ee63c0d5aaae06a04c
stacy1930 <35913788+stacy1930@users.noreply.github.com>
2020-01-09 18:25:03 +0100

    app et gant

--- Gant/gant.js
+++ Gant/gant.js
@@ -1,4 +1,4 @@
 // On récupère ce que le client "dit"
-const socket = io();
-let titre = "Aucun titre";
-let description = "Aucune description";
+// const socket = io();
+// let titre = "Aucun titre";
+// let description = "Aucune description";
--- app.js
+++ app.js
@@ -27,7 +27,7 @@ mongoClient.connect(url, function(err, db) {
 
   dbo.collection("gantt").find({}, function(err, result) {
     if (err) throw err;
-    // console.log(result.pseudo + " " + result.message);
+    console.log("pas ok");
   });
 
   //   ServerEvent.on("gantt", function(data) {
