45750eb896545fb8a02fd121aecbd369d57d97b4
stacy1930 <35913788+stacy1930@users.noreply.github.com>
2020-01-12 14:51:13 +0100

    test

--- app.js
+++ app.js
@@ -97,7 +97,6 @@ io.on("connection", client => {
   });
   // fin Envoie de tache en bdd
 
-<<<<<<< HEAD
   client.on("idToDelete", data => {
     mongoClient.connect(url, function(err, db) {
       if (err) console.log("SUPPRESSION");
@@ -112,9 +111,6 @@ io.on("connection", client => {
     });
   });
 
-=======
-  //RECUPERA5ION DES TACHES FIND
->>>>>>> 944e2340f029eb16989dd1d11058346e618d57a2
   mongoClient.connect(url, function(err, db) {
     if (err) console.log("ICI");
     let dbo = db.db("bddGantt");
@@ -158,9 +154,9 @@ io.on("connection", client => {
   console.log("server central connected");
   //********DEMANDE D'AIDE******** */
   //Pour demander de l'aide
-  clientTest.emit("needHelp");
+  // clientTest.emit("needHelp");
   //Ecouter la reponse de demande d'aide
-  clientTest.on("info", data => console.log(data));
+  // clientTest.on("info", data => console.log(data));
 
   //********RECEVOIR L'ENSEMBLE DE TOUS LES PROJETS******** */
   // clientTest.on("projectUpdated", data => console.log(data));
